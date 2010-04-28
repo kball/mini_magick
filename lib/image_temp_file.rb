ce4db0d194a910480bca6ba6f9a105bd28b34245
Kevin Ball <kball@causes.com>
2010-04-28 09:59:39 -0700

    Make certain to always have a unique tempfile name

--- lib/image_temp_file.rb
+++ lib/image_temp_file.rb
@@ -3,7 +3,14 @@ require "tempfile"
 module MiniMagick
   class ImageTempFile < Tempfile
     def make_tmpname(ext, n)
-      'mini_magick%d-%d%s' % [$$, n, ext ? ".#{ext}" : '']
+      t = Time.now.to_f
+      name = 'mini_magick%d-%d-%f%s' % [$$, n, t, ext ? ".#{ext}" : '']
+      i = 0
+      while File.exists?(name)
+        i += 1
+        name = 'mini_magick%d-%d-%f%d%s' % [$$, n, t, i, ext ? ".#{ext}" : '']
+      end
+      name
     end
   end
 end
